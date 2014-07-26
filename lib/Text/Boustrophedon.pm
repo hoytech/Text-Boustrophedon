d108a48a045a4bdbac063be125a997fcce101c0f
doug <doug@hcsw.org>
2014-07-26 01:09:13 -0400

    docs

--- lib/Text/Boustrophedon.pm
+++ lib/Text/Boustrophedon.pm
@@ -145,6 +145,8 @@ B<rongorongo> -- Reverse direction, rotate, and down-to-up direction
 
 NOTE: This module uses unicode characters that approximately look like their rotated or mirror-image equivalents (like L<Text::UpsideDown>) so the output values can contain "wide" characters.
 
+Finally, this module also includes the utility functions C<Text::Boustrophedon::mirror>, C<Text::Boustrophedon::flipped>, and C<Text::Boustrophedon::upsidedown> which you may find useful.
+
 
 =head1 SEE ALSO
 
